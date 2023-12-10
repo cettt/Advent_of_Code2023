5716780da7f86a4fcc776e4ca141ef539519f527
Cetin_G <ceting@gmx.at>
2023-12-10 21:50:00 +0100

    Solved day10 part2 using Pick's theorem and shoelace formula
    - Further performance improvements

--- day10.R
+++ day10.R
@@ -1,48 +1,30 @@
-data10 <- as.matrix(read.fwf("Input/day10.txt", widths = rep(1L, 140L)))
-n <- nrow(data10)*2L + 2L
-gr <- matrix("", nrow = n, ncol = n)
-gr[seq_len(nrow(data10)) * 2L, seq_len(nrow(data10)) * 2L] <- data10
+data10 <- unname(unlist(read.fwf("Input/day10.txt", widths = rep(1L, 140L))))
+tl <- list(L = c(-1i, 1), J = c(-1i, -1), "7" = c(1i, -1), F = c(1i, 1)) #turn list
 
+lp <- ((which(data10 == "S") - 1L) %% 140 + 1L)* 1i + which(data10 == "S") %/% 140L + 1L
+dir <- 1i #found manually by looking at input
 
-lp <- which(gr == "S")
-dir <- 1L
-
-while (TRUE) {
-  lp <- c(lp[1] + dir*2:1, lp)
-  cur <- gr[lp[1]]
-  
-  if (cur == "L") {
-    dir <- if (dir == 1L) n else -1L 
-  }
-  else if (cur == "J") {
-    dir <- if (dir == 1L) -n else -1L 
-  }
-  else if (cur == "7") {
-    dir <- if (dir == n) 1L else -n 
-  }
-  else if (cur == "F") {
-    dir <- if (dir == -1L) n else 1L 
+for (k in seq_len(1e5) + 1L) {
+  lp[k] <- lp[k - 1L] + dir
+  cur <- data10[(Re(lp[k]) - 1L) * 140 + Im(lp[k])]
+  if (cur %in% c("L", "J", "7", "F")) {
+    dir <- tl[[cur]][abs(Re(dir) + 2 * Im(dir))]
   } else if (cur == "S") break
+  
 }
-
 #part1-------
-length(lp[-1]) / 4L
+(k - 1L) / 2L
 
 #part 2-------
-find_adj <- function(k) {
-  m <- k %% n
-  k + c(if (k <= n^2 - n) n, if (k > n) -n, if (m != 1L) -1L, if (m != 0L) 1L)
-}
+ar <- sum((Im(lp)[-length(lp)] + Im(lp[-1])) * diff(Re(lp))) / 2L
+abs(ar) + 1L - (k - 1L) / 2L
 
-lookup <- sapply(seq_along(gr), find_adj)
-outside <- which(sapply(lookup, length) < 4L)
-cur <- outside
 
-while (length(cur) > 0L) {
-  nxt <- unique(unlist(lookup[cur]))
-  cur <- nxt[match(nxt, c(outside, lp), 0L) == 0L]
-  outside <- c(outside, cur)
-}
+#explanation
+# for part 1 we use a turn list (tl) to pick the new direction at each turn.
 
-x <- seq_along(gr)[-c(outside, lp)]
-sum((((x - 1L) %/% n) %% 2L == 1L) & x %% 2L == 0L)
+# for part 2 we use the Shoelace formula first to determine the are of the polygon,
+#   drawn by the loop.
+#   The formula is given by Area = 1/2 * sum_{i = 1}^n (y_i + y_{i+1}) * (x_i - x_{i+1})
+# Given the are of the polygon, we can use Pick's theorem:
+#   Area = interior + boundary/2 - 1, and solve for interior.
