4c5f0719273e40f1b68f2e796f843ea7f71a4f45
Cetin_G <ceting@gmx.at>
2023-12-11 12:44:35 +0100

    Optimized day 11

--- day11.R
+++ day11.R
@@ -1,20 +1,15 @@
 data11 <- read.fwf("Input/day11.txt", widths = rep(1, 140), comment.char = "")
 
-nr <- which(rowSums(data11 == "#") == 0L)
-nc <- unname(which(colSums(data11 == "#") == 0L))
+nr <- cumsum(rowSums(data11 == "#") == 0L)
+nc <- cumsum(colSums(data11 == "#") == 0L)
 
-gal <- apply(unname(which(data11 == "#", arr.ind = TRUE)), 1, \(x) x, simplify = FALSE)
+gal <- which(data11 == "#", arr.ind = TRUE, useNames = FALSE)
+gal <- t(cbind(gal, nr[gal[, 1]], nc[gal[, 2]]))
 
-find_d <- function(x){
-  y1 <- pmin(x[[1]], x[[2]])
-  y2 <- pmax(x[[1]], x[[2]])
-  c(sum(y2 - y1), sum(y1[1] <= nr & nr <= y2[1]) + sum(y1[2] <= nc & nc <= y2[2]))
-}
-
-res <- combn(gal, 2, find_d2)
+res <- apply(gal, 2L, \(x) colSums(matrix(rowSums(abs(gal - x)), nrow = 2L)))
 
 #part1-------
-sum(res)
+sum(res) / 2L
 
 #part2-------
-sum(res * c(1L, 1e6 - 1L))
+sum(res * c(1L, 1e6 - 1L)) / 2L
