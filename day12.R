c611b14cac316b4df760b66db3204613a362f050
Cetin_G <ceting@gmx.at>
2023-12-15 22:16:42 +0100

    Added explanation to day12

--- day12.R
+++ day12.R
@@ -4,13 +4,13 @@ tile2int <- c("." = 0L, "#" = 1L, "?" = 2L)
 map_springs <- function(x, n = 1L) {
   y <- tile2int[c(strsplit(paste(rep(x[1], n), collapse = "?"), "")[[1]], ".")]
   list(
-    matrix(c(
+    df = matrix(c(
       unname(y),
       rev(cumsum(rev(y) != 0L)), #number of remaining (potentially) non-operating springs
       as.integer(sapply(seq_along(y), \(k) sum(sign(cumprod(y[k:length(y)]))))), #size of largest possible group,
       sapply(seq_along(y), \(k) Position(\(z) z, y[k:length(y)] == 1L, nomatch = 1e5L))# nr of tiles until first damaged spring
     ), ncol = 4), 
-    rep(as.integer(strsplit(x[2], ",")[[1]]), n) 
+    n = rep(as.integer(strsplit(x[2], ",")[[1]]), n) 
   )
 }
 
@@ -40,6 +40,7 @@ count_pos <- function(k = 1L, j = 1L) {
 #read data----
 springs <- lapply(c(1L, 5L), \(n) lapply(data12, map_springs, n = n))
 
+#part 1 and 2--------
 res <- c(0L, 0L)
 for (k in seq_along(springs2)) {
   for (p in 1:2){
@@ -52,3 +53,25 @@ for (k in seq_along(springs2)) {
 }
 
 sprintf("%.f", res)
+
+
+#Explanation------
+# First, for a given line we compute the following:
+#   - a matrix df with four columns
+#     - column 1 are the tiles converted to integers (necessary to have an integer matrix)
+#     - column 2 the maximum number of non-operating tiles possible from a given position to the end
+#     - column 3 the maximum number of damaged tiles that could start at that tile
+#     - column 4 the first time after a given tile, that a damaged tile appears
+#  - an integer vector, containing the group sizes
+
+# We use a recursive function which does the following:
+#  - Given that we are at position k and still have to find all groups starting from j
+#     - Find all possible positions (pos) for the next group (of size n0[j])
+#     - If no such position can be found return 0
+#     - Otherwise increase the current position (k) by n0[j] + pos and increase j by 1 and call the function again.
+# - The functions aborts if we either
+#   - go past the last position, or
+#   - have found all the groups 
+
+# Memoization can be used to speed up (especially for part2)
+#  We use k*100L + j as index and store results in an integer vector.
