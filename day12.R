f57efff18846d6c527204ac1a531b9dd590f8374
Cetin_G <ceting@gmx.at>
2023-12-15 22:01:23 +0100

    Fast solution for day 12

--- day12.R
+++ day12.R
@@ -1,69 +1,54 @@
-f <- function(x) {
-  x <- strsplit(x, " ")[[1]]
-  y <- strsplit(x[1], "")[[1]] 
+data12 <- strsplit(readLines("Input/day12.txt"),  " ") 
+tile2int <- c("." = 0L, "#" = 1L, "?" = 2L)
+
+map_springs <- function(x, n = 1L) {
+  y <- tile2int[c(strsplit(paste(rep(x[1], n), collapse = "?"), "")[[1]], ".")]
   list(
-    c(y, "."), 
-    as.integer(strsplit(x[2], ",")[[1]]),  
-    c(rev(cumsum(rev(y) != ".")), 0),
-    sapply(seq_along(y), \(k) sum(cumprod(y[k:length(y)] != ".")))
+    matrix(c(
+      unname(y),
+      rev(cumsum(rev(y) != 0L)), #number of remaining (potentially) non-operating springs
+      as.integer(sapply(seq_along(y), \(k) sum(sign(cumprod(y[k:length(y)]))))), #size of largest possible group,
+      sapply(seq_along(y), \(k) Position(\(z) z, y[k:length(y)] == 1L, nomatch = 1e5L))# nr of tiles until first damaged spring
+    ), ncol = 4), 
+    rep(as.integer(strsplit(x[2], ",")[[1]]), n) 
   )
 }
 
-data12 <- lapply(readLines("Input/day12.txt"), f)
 
-check_pos <- function(k, y, mx) {
-  if (all(y[k + 1:mx - 1L] != ".") & (if (k + mx <= length(y)) y[k + mx] != "#" else TRUE)) k else -1L 
-}
-
-solve <- function(x) {
-
-  n <- x[[2]]
-  y <- x[[1]]
-  rem <- x[[3]]
-  a <- x[[4]]
-  if (length(n) == 0L) {
-    if (any(y == "#")) return(0L) else return(1L)
+count_pos <- function(k = 1L, j = 1L) {
+  if (k > nrow(df0)) return(if (j <= length(n0)) 0L else 1L)
+  if (j  > length(n0)) return(if (any(df0[k:nrow(df0), 1L] == 1L)) 0L else 1L)
+  if (lookup[k*100L + j] >= 0L) return(lookup[k*100L + j])
+  
+  df <- df0[k:nrow(df0), ,drop = F]
+  n  <- n0[j:length(n0)]
+  
+  if (df[1, 2] < sum(n)) {
+    res <- 0L
+  } else {
+    pos <- seq_len(min(df[1L, 4L], sum(df[, 2L] >= sum(n))))
+    pos <- pos[df[pos, 3L] >= n[1L] & df[pos + n[1L], 1L] != 1L]
+    if (length(pos) == 0L) return(0L)
+    
+    res <- sum(sapply(pos, \(i) count_pos(k + i + n[1], j + 1L)))
   }
-  if (all(rem < sum(n))) return(0L)
-  k_first <- which.max(y != ".")
-  k_last1 <- sum(rem >= sum(n))
-  k_last <- max(k_first, min(Position(\(y) y, y == "#", nomatch = length(y)), k_last1))
-  pos <- seq.int(k_first, k_last)
-  pos <- pos[a[pos] >= n[1] & y[pos + n[1]] != "#"]
-  # pos <- sapply(seq.int(k_first, k_last), check_pos, y = y, mx = n[1])
-
-  if (length(pos) == 0L) return(0L)
   
-  sum(sapply(pos, \(k) {
-    xnew <- x
-    xnew[[1]] <- xnew[[1]][-seq_len(k + n[1])]
-    xnew[[2]] <- xnew[[2]][-1]
-    xnew[[3]] <- xnew[[3]][-seq_len(k + n[1])]
-    xnew[[4]] <- xnew[[4]][-seq_len(k + n[1])]
-    solve(xnew)
-  }))
+  lookup[k*100L + j] <<- res
+  return(res)
 }
 
-solve(data12[[1]])
+#read data----
+springs <- lapply(c(1L, 5L), \(n) lapply(data12, map_springs, n = n))
 
-part1_new <- sapply(data12, solve)
-sum(part1_new)
-
-
-#part2------
-
-#brute force ftw------
-f2 <- function(x) {
-  x <- strsplit(x, " ")[[1]]
-  y <- strsplit(paste(rep(x[1], 5L), collapse = "?"), "")[[1]]
-  
-  list(
-    y, 
-    as.integer(strsplit(paste(rep(x[2], 5), collapse = ","), ",")[[1]]),  
-    rev(cumsum(rev(y) != "."))
-  )
+res <- c(0L, 0L)
+for (k in seq_along(springs2)) {
+  for (p in 1:2){
+    lookup <- rep.int(-1L, 1e5)
+    x <- springs2[[k]]
+    df0 <- springs[[p]][[k]][[1]]
+    n0 <-  springs[[p]][[k]][[2]]
+    res[p] <- res[p] + count_pos(1L, 1L)
+  }
 }
 
-data12_b <- lapply(readLines("Input/day12.txt"), f2)
-
-solve(data12_b[[2]])
+sprintf("%.f", res)
